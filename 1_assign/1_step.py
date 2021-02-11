3a7fc3cb0e9483b685ffe545912d026c05f30bf3
Brandon Essler <Essler@users.noreply.github.com>
2021-02-11 17:07:04 -0600

    Remove unnecessary code and imports

--- 1_assign/1_step.py
+++ 1_assign/1_step.py
@@ -1,16 +1,9 @@
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing import image
 
 # 32x32px RGB images, labels 0-9
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-
-# 10 classes for CIFAR10, 0-9 for airplane, automobile, bird, cat, deer, dog, frog, horse, ship, & truck
-num_classes = 10
-
-# CIFAR10 contains 32x32px RGB images
-img_rows, img_cols = 32, 32
 
 # Change data type to 32-bit floats and normalize colors by dividing by 255 (the largest 8-bit color value)
 x_train = x_train.astype('float32')
@@ -23,4 +16,4 @@ print(x_test.shape[0], ' test samples')
 
 # Display 16 example images
 for i in range(16):
-    tf.keras.preprocessing.image.array_to_img(x_train[i]).show()
+    image.array_to_img(x_train[i]).show()
