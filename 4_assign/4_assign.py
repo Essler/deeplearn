191c70fe18c2095f55c42bbe8f868b0bb2194ed1
Essler <Essler@users.noreply.github.com>
2021-04-24 23:34:00 -0500

    Freeze pre-trained weights

--- 4_assign/4_assign.py
+++ 4_assign/4_assign.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 
 def run():
+    # Load VGG16 with pre-trained weights on ImageNet, including classification top.
     vgg16 = tf.keras.applications.VGG16(
         include_top=True,
         weights="imagenet",
@@ -12,7 +13,12 @@ def run():
         classifier_activation="softmax",
     )
 
+    # Output the model.
     vgg16.summary()
+
+    # Freeze all pre-trained weights, enforcing training for only the inserted STN attention modules.
+    for layer in vgg16.layers:
+        layer.trainable = False
 
 
 if __name__ == '__main__':
